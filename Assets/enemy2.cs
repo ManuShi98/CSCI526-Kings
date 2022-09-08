93ba13c54ae38752e7ca6594f71e659d3717d6d2
CodingNie <97939578+CodingNie@users.noreply.github.com>
2022-09-08 13:43:19 -0700

    version1

---
+++ Assets/enemy2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy2 : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    void Start() {
+        target = path2.points[0];    
+    }
+
+    void Update() { 
+        if(transform.position == target.position) {
+            GetNextPath2Point();
+        } else {
+            Vector2 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void GetNextPath2Point() {
+        if (wavePointIndex >= path2.points.Length-1) {
+            Destroy(gameObject);
+            return;
+        }
+        wavePointIndex++;
+        target = path2.points[wavePointIndex];
+    }
+}
---
+++ Assets/enemy3.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy3 : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    void Start() {
+        
+            Destroy(gameObject);
+
+        target = path3.points[0];    
+    }
+
+    void Update() { 
+        if(transform.position == target.position) {
+            GetNextPath3Point();
+        } else {
+            Vector2 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void GetNextPath3Point() {
+        if (wavePointIndex >= path3.points.Length-1) {
+            Destroy(gameObject);
+            return;
+        }
+        wavePointIndex++;
+        target = path3.points[wavePointIndex];
+    }
+}
