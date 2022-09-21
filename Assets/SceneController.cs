bbd48b65b8c67b90cbaa325c80975a1c679aa364
yuanhao mao <yuanhaom@usc.edu>
2022-09-21 13:24:29 -0700

    feat(move tower): Add tower panel prefab

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -15,6 +15,11 @@ public class SceneController : MonoBehaviour
 
     private Dictionary<TileBase, TileData> dataFromTiles;
 
+    // [SerializeField]
+    // private GameObject towerPrefab;
+
+    private TowerBtn towerBtn;
+
     private void Awake()
     {
         dataFromTiles = new Dictionary<TileBase, TileData>();
@@ -119,11 +124,13 @@ public class SceneController : MonoBehaviour
             foreach(var map in maps)
             {
                 Vector3Int gridPosition = map.WorldToCell(mousePosition);
+                // Vector3 gridCenterPosition = map.GetCellCenterWorld(gridPosition);
                 TileBase clickedTile = map.GetTile(gridPosition);
                 if(clickedTile != null)
                 {
                     string tag = dataFromTiles[clickedTile].tag;
                     print("At position " + gridPosition + " " + tag);
+                    // PlaceTower(gridCenterPosition, tag);
                     break;
                 }
             }
@@ -150,4 +157,26 @@ public class SceneController : MonoBehaviour
     {
         return currentWeather;
     }
+
+    // public void PlaceTower(Vector3Int gridPosition, string tag)
+    // {
+    //   if (!EventSystem.current.IsPointerOverGameObject() && this.ClickedBtn != null)
+    //   {
+    //     if (string.Compare(tag, "\"wall\"") == 0)
+    //     {
+    //       Instantiate(towerPrefab, gridPosition, Quaternion.identity);
+    //     }
+    //     else
+    //     {
+    //       print("You cannot build tower on the path!");
+    //     }
+    //     this.ClickedBtn = null;
+    //   }
+
+    // }
+
+    // public void PickTower(TowerBtn towerBtn)
+    // {
+    //   this.ClickedBtn = towerBtn;
+    // }
 }
