051a08a4f256094e413a5412ec1e35227741d0ab
LiuDan98 <dliu3405usc@gmail.com>
2022-09-21 22:07:20 -0700

    feat(enemy): Add enemy path and generator

--- Assets/Prefabs/Enemy/Enemy3.cs
+++ Assets/Prefabs/Enemy/Enemy3.cs
@@ -2,27 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy : MonoBehaviour {
-
-    //public float speed;
-
-    //private Transform target;
+public class Enemy3 : MonoBehaviour
+{
     public float speed = 10;
     private Transform[] positions;
     private int index = 0;
 
-    void Start() {
-        positions = Path1.positions1;
+    void Start()
+    {
+        positions = Path3.positions3;
     }
 
-    void Update () {
+    void Update()
+    {
         Move();
     }
 
-    void Move() {
+    void Move()
+    {
         if (index > positions.Length - 1) return;
         transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
-        if (Vector3.Distance(positions[index].position, transform.position) < 0.3f) {
+        if (Vector3.Distance(positions[index].position, transform.position) < 0.3f)
+        {
             index++;
         }
     }
---
+++ Assets/Prefabs/Enemy/Wave.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject enemyPrefab;
+    public int count;
+    public float rate;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
