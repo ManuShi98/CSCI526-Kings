f2b3e5794ff617748443dd348eb091be66ce6819
JoernZheng <474615252@qq.com>
2022-09-28 08:08:27 -0700

    bugfix(tower): 1.Fixed tower rotation issue; 2.Fixed bullet direction issue.

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,34 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-  public float speed = 10f;
-  public float damage = 20f;
-  public Rigidbody2D bullet;
+    public float speed = 10f;
+    public float damage = 20f;
+    public Rigidbody2D bullet;
 
-  void Start()
-  {
-    bullet.velocity = transform.up * speed;
-  }
-
-  public void OnBecameInvisible()
-  {
-    Destroy(gameObject);
-  }
-
-  private void OnTriggerEnter2D(Collider2D collision)
-  {
-    if (!collision.CompareTag("Enemy"))
-      return;
-
-    EnemyUnit enemy = collision.GetComponent<EnemyUnit>();
-
-    if (enemy.health > 0)
+    /// <summary>
+    /// Open fire. The initial rotation refers to the fire point's rotation.
+    /// And bullets always fly from the right direction,
+    /// so the bullet prefabs need to guarantee their head is to the right side.
+    /// </summary>
+    /// <param name="rotation"></param>
+    public void fire(Quaternion rotation)
     {
-      enemy.TakeDamage(damage);
+        transform.rotation = rotation;
+        bullet.velocity = transform.right * speed;
+    } 
+
+    public void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
-    Destroy(gameObject);    // gameObject指代自身
-  }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy"))
+            return;
+
+        EnemyUnit enemy = collision.GetComponent<EnemyUnit>();
+
+        if (enemy.health > 0)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
 }
