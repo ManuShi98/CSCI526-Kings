5049ea70ea0f46639209b0ce6e17183c70330c42
yuanhao mao <yuanhaom@usc.edu>
2022-09-28 16:00:47 -0700

    feat(coin system): Coin system with tower

--- Assets/Prefabs/TowerPanel/TowerBtn.cs
+++ Assets/Prefabs/TowerPanel/TowerBtn.cs
@@ -1,10 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerBtn : MonoBehaviour
 {
   public GameObject towerPrefab;
 
   public Sprite sprite;
+
+  [SerializeField]
+  private int price = 1;
+
+  private Button Btn;
+
+  void Start()
+  {
+    Btn = this.GetComponent<Button>();
+  }
+
+  void Update()
+  {
+    ChangeBtnStatus();
+  }
+
+  public int GetPrice()
+  {
+    return price;
+  }
+
+  public void ChangePrice(int newPrice)
+  {
+    price = newPrice;
+  }
+
+  private void ChangeBtnStatus()
+  {
+    int currentCoins = GamingDataController.getInstance().getCoinCount();
+    if (currentCoins < price)
+    {
+      Btn.enabled = false;
+    }
+    else
+    {
+      Btn.enabled = true;
+    }
+  }
 }
