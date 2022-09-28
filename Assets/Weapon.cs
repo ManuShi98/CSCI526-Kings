b8933278398d891c0f01d9d80cbff5a90f815b9f
yuanhao mao <yuanhaom@usc.edu>
2022-09-28 16:02:16 -0700

    feat(coin system): Coin system with enemy and tower. Season change's effect on weapons. TODO: radius

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -2,75 +2,98 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public double radius = 100; // Weapon's firing radius
-    public double FiringRate = 10;  // Fire per second
-    public float damage = 20f;
+  public double radius = 100; // Weapon's firing radius
+  public double FiringRate = 10;  // Fire per second
 
-    public Transform firePoint;
+  [SerializeField]
+  private float startDamage = 20f;
+  private float damage = 20f;
 
-    public GameObject enemy; // Locked enemy instance
-    public GameObject bulletPrefab;
+  public Transform firePoint;
 
-    private double FiringIntervalTime;
-    private double timer;
+  public GameObject enemy; // Locked enemy instance
+  public GameObject bulletPrefab;
 
-    void Start()
+  private double FiringIntervalTime;
+  private double timer;
+
+  void Start()
+  {
+    damage = startDamage;
+    FiringIntervalTime = 1.0 / FiringRate;
+    timer = FiringIntervalTime;
+
+    SceneController.OnSeasonChangeHandler += ReceiveSeasonChangedValue;
+    ReceiveSeasonChangedValue(gameObject, new SeasonArgs(SceneController.GetSeason().ToString().ToLower()));
+  }
+
+  void Update()
+  {
+    if (enemy != null)
     {
-        FiringIntervalTime = 1.0 / FiringRate;
+      // 旋转Fire Point
+      Vector2 dir = enemy.transform.position - transform.position;
+      float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+      firePoint.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+      // 射击
+      timer -= Time.deltaTime;
+      if (timer <= 0)
+      {
+        Bullet newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation).GetComponent<Bullet>();
+        newBullet.damage = damage;
+        newBullet.fire(firePoint.rotation);
         timer = FiringIntervalTime;
+      }
     }
-
-    void Update()
+    else
     {
-        if (enemy != null)
+      // TODO: 临时方法。后期更换为怪物控制器 + Map(GameObject ID, Health)的方式控制目标锁定方式
+      // 优先攻击距离最近的敌人
+      GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+      double minDistance = double.MaxValue;
+      foreach (GameObject e in enemies)
+      {
+        double currDistance = TwoPointDistance2D(e.transform.position, transform.position);
+        if (currDistance < minDistance)
         {
-            // 旋转Fire Point
-            Vector2 dir = enemy.transform.position - transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            firePoint.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-            // 射击
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                Bullet newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation).GetComponent<Bullet>();
-                newBullet.damage = damage;
-                newBullet.fire(firePoint.rotation);
-                timer = FiringIntervalTime;
-            }
+          minDistance = currDistance;
+          enemy = e;
         }
-        else
-        {
-            // TODO: 临时方法。后期更换为怪物控制器 + Map(GameObject ID, Health)的方式控制目标锁定方式
-            // 优先攻击距离最近的敌人
-            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            double minDistance = double.MaxValue;
-            foreach (GameObject e in enemies)
-            {
-                double currDistance = TwoPointDistance2D(e.transform.position, transform.position);
-                if (currDistance < minDistance)
-                {
-                    minDistance = currDistance;
-                    enemy = e;
-                }
-            }
-        }
+      }
     }
+  }
 
-    /// <summary>
-    /// Calculate the distance between two points in 2D game
-    /// </summary>
-    /// <param name="p1"></param>
-    /// <param name="p2"></param>
-    /// <returns></returns>
-    private float TwoPointDistance2D(Vector2 p1, Vector2 p2)
-    {
-        return Mathf.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-    }
+  /// <summary>
+  /// Calculate the distance between two points in 2D game
+  /// </summary>
+  /// <param name="p1"></param>
+  /// <param name="p2"></param>
+  /// <returns></returns>
+  private float TwoPointDistance2D(Vector2 p1, Vector2 p2)
+  {
+    return Mathf.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }
 
-    private void ReceiveSeasonChangedValue(object sender, System.EventArgs args)
+  private void ReceiveSeasonChangedValue(object sender, System.EventArgs args)
+  {
+    print("change season");
+    SeasonArgs seasonArgs = (SeasonArgs)args;
+    if (seasonArgs.CurrentSeason == "spring")
     {
-        SeasonArgs seasonArgs = (SeasonArgs)args;
-        Debug.Log(seasonArgs.CurrentSeason);
+      damage *= 0.7f;
     }
+    else if (seasonArgs.CurrentSeason == "summer")
+    {
+      damage = startDamage;
+    }
+    else if (seasonArgs.CurrentSeason == "autumn")
+    {
+      damage = startDamage;
+    }
+    else if (seasonArgs.CurrentSeason == "winter")
+    {
+      damage = startDamage;
+    }
+  }
 }
