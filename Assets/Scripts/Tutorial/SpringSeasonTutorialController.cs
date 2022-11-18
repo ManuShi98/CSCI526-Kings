82ec74f1b1c06fd14975235ad1e54c4a70b024a6
JoernZheng <474615252@qq.com>
2022-11-18 00:56:14 -0800

    Add the energy bar tutorial

--- Assets/Scripts/Tutorial/SpringSeasonTutorialController.cs
+++ Assets/Scripts/Tutorial/SpringSeasonTutorialController.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using TransformUtil;
 
-public class SpringSeasonTutorialController : MonoBehaviour, IEventHandler<SeasonChangeEvent>, IEventHandler<CollidersClickEvent>, IEventHandler<GameStartEvent>
+public class SpringSeasonTutorialController : MonoBehaviour, IEventHandler<CollidersClickEvent>, IEventHandler<GameStartEvent>
 {
     private int step;
     private Transform arrow1;
     private Transform arrow2;
     private Transform arrow3;
+    private Transform arrow4;
     private float firstX;
     private float secondY;
     private float thirdY;
+    private float forthY;
 
     private float timer = 3;
 
@@ -19,17 +21,16 @@ public class SpringSeasonTutorialController : MonoBehaviour, IEventHandler<Seaso
 
     private void OnEnable()
     {
-        EventBus.register<SeasonChangeEvent>(this);
         EventBus.register<CollidersClickEvent>(this);
         EventBus.register<GameStartEvent>(this);
     }
 
     private void OnDisable()
     {
-        EventBus.unregister<SeasonChangeEvent>(this);
         EventBus.unregister<CollidersClickEvent>(this);
         EventBus.unregister<GameStartEvent>(this);
     }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,10 +42,12 @@ public class SpringSeasonTutorialController : MonoBehaviour, IEventHandler<Seaso
         arrow1 = transform.FindDeepChild("Arrow 1");
         arrow2 = transform.FindDeepChild("Arrow 2");
         arrow3 = transform.FindDeepChild("Arrow 3");
+        arrow4 = transform.FindDeepChild("Arrow 4");
 
         firstX = arrow1.position.x;
         secondY = arrow2.position.y;
         thirdY = arrow3.position.y;
+        forthY = arrow4.position.y;
     }
 
     // Update is called once per frame
@@ -62,45 +65,46 @@ public class SpringSeasonTutorialController : MonoBehaviour, IEventHandler<Seaso
         {
             arrow3.position = new Vector3(arrow3.position.x, Mathf.PingPong(Time.time, 0.5f) + thirdY, arrow3.position.z);
         }
+        if (arrow4 != null && arrow4.gameObject.activeSelf == true)
+        {
+            arrow4.position = new Vector3(arrow4.position.x, Mathf.PingPong(Time.time, 0.5f) + forthY, arrow4.position.z);
+        }
 
-        if(timer >= 0)
+        if (timer >= 0 && (arrow1.gameObject.activeInHierarchy || arrow2.gameObject.activeInHierarchy))
         {
             timer -= Time.deltaTime;
-        } else if(arrow1.gameObject.activeInHierarchy)
+        }
+        else if (arrow1.gameObject.activeInHierarchy)
         {
             step = 2;
             arrow1.gameObject.SetActive(false);
             arrow2.gameObject.SetActive(true);
-            Debug.Log("active arrow 2");
-        }
-    }
-
-    public void HandleEvent(SeasonChangeEvent eventData)
-    {
-        //if (step == 1 && eventData.ChangedSeason == Season.SPRING)
-        //{
-        //    step = 2;
-        //    arrow1.gameObject.SetActive(false);
-        //    arrow2.gameObject.SetActive(true);
-        //}
-    }
-
-    public void HandleEvent(CollidersClickEvent eventData)
-    {
-        if (eventData.obj == targetTower && step == 2)
+            timer = 3;
+        } else if(arrow2.gameObject.activeInHierarchy)
         {
             step = 3;
             arrow2.gameObject.SetActive(false);
             arrow3.gameObject.SetActive(true);
+            UIManager.unblockTower(targetTower);
+        }
+    }
+
+    public void HandleEvent(CollidersClickEvent eventData)
+    {
+        if (eventData.obj == targetTower && step == 3)
+        {
+            step = 4;
+            arrow3.gameObject.SetActive(false);
+            arrow4.gameObject.SetActive(true);
         }
     }
 
     public void HandleEvent(GameStartEvent eventData)
     {
-        step = 4;
+        step = 5;
         arrow1.gameObject.SetActive(false);
-        arrow2.gameObject.SetActive(false);
         arrow3.gameObject.SetActive(false);
+        arrow4.gameObject.SetActive(false);
         foreach (var tower in blockTowers)
         {
             UIManager.unblockTower(tower);
